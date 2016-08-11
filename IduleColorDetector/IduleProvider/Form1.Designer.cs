688f7d881edf6aec6730405ec3ae9fd76cb180cc
Chuan Chen <riverchen@gmail.com>
2016-08-11 15:41:12 -0400

    Added idule project.

---
+++ IduleColorDetector/IduleProvider/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace IduleProvider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorCheckbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.saveRawImgButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.displayAdapter2 = new Awaiba.Controls.DisplayAdapter();
+            this.displayAdapter1 = new Awaiba.Controls.DisplayAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorCheckbox
+            // 
+            this.colorCheckbox.AutoSize = true;
+            this.colorCheckbox.Checked = true;
+            this.colorCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.colorCheckbox.Location = new System.Drawing.Point(17, 171);
+            this.colorCheckbox.Name = "colorCheckbox";
+            this.colorCheckbox.Size = new System.Drawing.Size(125, 17);
+            this.colorCheckbox.TabIndex = 13;
+            this.colorCheckbox.Text = "Color Reconstruction";
+            this.colorCheckbox.UseVisualStyleBackColor = true;
+            this.colorCheckbox.CheckedChanged += new System.EventHandler(this.colorCheckbox_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Pre-Scaler";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(68, 130);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 25);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // saveRawImgButton
+            // 
+            this.saveRawImgButton.Location = new System.Drawing.Point(12, 41);
+            this.saveRawImgButton.Name = "saveRawImgButton";
+            this.saveRawImgButton.Size = new System.Drawing.Size(156, 23);
+            this.saveRawImgButton.TabIndex = 10;
+            this.saveRawImgButton.Text = "Take Snapshot";
+            this.saveRawImgButton.UseVisualStyleBackColor = true;
+            this.saveRawImgButton.Click += new System.EventHandler(this.saveRawImgButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(93, 12);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 9;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 7;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Record Processed Video";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Record Raw Video";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // displayAdapter2
+            // 
+            this.displayAdapter2.Location = new System.Drawing.Point(840, 12);
+            this.displayAdapter2.Name = "displayAdapter2";
+            this.displayAdapter2.Size = new System.Drawing.Size(640, 640);
+            this.displayAdapter2.TabIndex = 19;
+            // 
+            // displayAdapter1
+            // 
+            this.displayAdapter1.Location = new System.Drawing.Point(194, 12);
+            this.displayAdapter1.Name = "displayAdapter1";
+            this.displayAdapter1.Size = new System.Drawing.Size(640, 640);
+            this.displayAdapter1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1538, 678);
+            this.Controls.Add(this.displayAdapter2);
+            this.Controls.Add(this.displayAdapter1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.colorCheckbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.saveRawImgButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox colorCheckbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button saveRawImgButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private Awaiba.Controls.DisplayAdapter displayAdapter1;
+        private Awaiba.Controls.DisplayAdapter displayAdapter2;
+    }
+}
+
---
+++ IduleColorDetector/IduleProvider/Form1.cs
@@ -0,0 +1,245 @@
+﻿///Use the define that is good for your needs:
+///IduleCam0 -> Get data from the first (or only) camera that is connected
+///IduleCam1 -> Get data from the second camera that is connected. If only one camera is connected, then this will give an error
+///IduleStereo -> Get data from two cameras simultaneously
+
+
+//#define IduleCam0_raw
+//#define IduleCam0_processed
+//#define IduleCam1_raw
+//#define IduleCam1_processed
+//#define IduleStereo_raw
+#define IduleStereo_processed
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Awaiba.Drivers.Grabbers;
+using Awaiba.FrameProcessing;
+using System.Runtime.InteropServices;
+
+namespace IduleProvider
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Camera Initialization
+        /// IduleProviderCsCam(0) -> initialize first camera that is connected
+        /// IduleProviderCsCam(1) -> initialize the second camera that is connected (in case you want to see images just from the second camera)
+        /// StereoIduleProviderCs -> To see images from two cameras (you need to have two cameras connected)
+        /// </summary>
+
+#if(IduleCam0_raw || IduleCam0_processed)
+        IduleProviderCsCam provider = new IduleProviderCsCam(0);
+#endif
+
+#if(IduleCam1_raw || IduleCam1_processed)
+        IduleProviderCsCam provider = new IduleProviderCsCam(1);
+#endif
+
+#if(IduleStereo_raw || IduleStereo_processed)
+        StereoIduleProviderCs provider = new StereoIduleProviderCs();
+#endif
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Note: To receive the 90 fps that the sensor sends, only the "ImageTransaction" hook needs to be done;
+            //The ImageProcessed event does the image processing, and it can only manage around 30 fps;
+            
+            provider.Exception += provider_Exception;
+            provider.Initialize();
+
+#if(IduleCam0_raw)
+            provider.ImageTransaction += provider_ImageTransaction;
+#endif
+#if(IduleCam0_processed)
+            provider.ImageProcessed += provider_ImageProcessed;
+#endif
+#if(IduleCam1_raw)
+            provider.ImageTransaction += provider_ImageTransaction;
+#endif
+#if(IduleCam1_processed)
+            provider.ImageProcessed += provider_ImageProcessed;
+#endif
+#if(IduleStereo_raw)
+            provider.ImageTransaction += provider_ImageTransaction;
+#endif
+#if(IduleStereo_processed)
+            provider.ImageProcessed += provider_ImageProcessed;
+            provider.ImageProcessed2 += provider_ImageProcessed;
+#endif
+            provider.WriteRegister(new NanEyeGSRegisterPayload(false, 0x05, true, 0, 50));
+            provider.WriteRegister(new NanEyeGSRegisterPayload(false, 0x06, true, 0, 255));
+
+            provider.Exception += camera_Exception;
+            FormClosing += Form1_FormClosing;
+
+            //ProcessingWrapper.pr[0].ReduceProcessingColor = false;
+            //ProcessingWrapper.pr[0].skipFrames.FramesNumber = 4;
+        }
+
+        private void provider_Exception(object sender, OnExceptionEventArgs e)
+        {
+            Console.WriteLine("Exception: " + e.ex.Message);
+        }
+
+        /// <summary>
+        /// Displays the image after the image processing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void provider_ImageProcessed(object sender, OnImageReceivedBitmapEventArgs e)
+        {
+            Awaiba.Imaging.PPMImage ppm = new Awaiba.Imaging.PPMImage(e.Width, e.Height, e.BitsPerPixel, e.PixelData);
+
+            if (e.SensorID == 0)
+                displayAdapter1.DrawImage(ppm);
+
+            if(e.SensorID == 1)
+                displayAdapter2.DrawImage(ppm);
+        }
+
+        /// <summary>
+        /// Displays the raw 8 bit image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void provider_ImageTransaction(object sender, ImageReceivedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(
+                    new Action<object, Awaiba.Drivers.Grabbers.ImageReceivedEventArgs>(provider_ImageTransaction),
+                        new object[] { sender, e });
+            }
+            else
+            {
+                Awaiba.Imaging.PGMImage ppm = new Awaiba.Imaging.PGMImage(e.Width, e.Height, e.BitsPerPixel, e.PixelData);
+                if (e.SensorID == 0)
+                    displayAdapter1.DrawImage(ppm);
+
+                if (e.SensorID == 1)
+                    displayAdapter2.DrawImage(ppm);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            provider.Dispose();
+        }
+
+        /// <summary>
+        /// If the camera gets into any exception, it will be thrown in this method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void camera_Exception(object sender, OnExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ex.Message);
+        }
+
+        /// <summary>
+        /// Example on how to use the Awaiba's image processing library
+        /// More information on Awaiba's Support website on the "Awaiba Image Processing" document
+        /// This can only be used in the Processed Images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void colorCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            ProcessingWrapper.pr[0].colorReconstruction.Apply = colorCheckbox.Checked;
+        }
+
+        /// <summary>
+        /// Start getting images from the camera
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            provider.StartCapture();
+        }
+
+        /// <summary>
+        /// To stop getting images from the camera
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            provider.StopCapture();
+        }
+
+        /// <summary>
+        /// Method to save an image
+        /// This can only be used in the Processed Images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveRawImgButton_Click(object sender, EventArgs e)
+        {
+            ProcessingWrapper.pr[0].TakeSnapshost().rawImage.Save("image.pgm");
+            ProcessingWrapper.pr[0].TakeSnapshost().processedImage.Save("image.png");
+        }
+
+        /// <summary>
+        /// Example how to send registers to the camera
+        /// In this case, changing the pre-scaler (0x05) value;
+        /// Also available to change the exposure (0x06)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            provider.WriteRegister(new NanEyeGSRegisterPayload(true, 0x05, true, 0, trackBar1.Value));
+        }
+
+        /// <summary>
+        /// Method to record raw videos
+        /// This can only be used in the Processed Images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "Record Raw Video")
+            {
+                ProcessingWrapper.pr[0].awRawVideo.StartVideo("Raw.awvideo");
+                button4.Text = "Stop Recording...";
+            }
+            else if (button4.Text == "Stop Recording...")
+            {
+                ProcessingWrapper.pr[0].awRawVideo.Close();
+                button4.Text = "Record Raw Video";
+            }
+        }
+
+        /// <summary>
+        /// Method to record processed videos (after the Awaiba Image Processing Pipeline)
+        /// This can only be used in the Processed Images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Record Processed Video")
+            {
+                ProcessingWrapper.pr[0].awVideo.StartVideo(20, "Processed.avi", true);
+                button5.Text = "Stop Recording...";
+            }
+            else if (button5.Text == "Stop Recording...")
+            {
+                ProcessingWrapper.pr[0].awVideo.StopVideo();
+                button5.Text = "Record Processed Video";
+            }
+        }
+    }
+}
